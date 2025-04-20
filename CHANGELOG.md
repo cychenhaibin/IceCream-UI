41c18dca81eedd524c794ff06793001bc3b06b67
haibin.chen <haibin.chen@shopee.com>
2025-04-20 14:35:33 +0800

    feat：Tooltip组件

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,1 +1,5 @@
-2025.4.19 @cychenhaibin 搭建组件库并编写button组件和message组件
+2025.4.19 @cychenhaibin feat：搭建组件库并编写button组件和message组件
+
+2025.4.20 @cychenhaibin fix：目录结构
+
+2025.4.20 @cychenhaibin feat：Tooltip组件
--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export { default as Foo } from './Foo';
 export { default as Button } from './Basic/Button';
 export { default as message } from './Feedback/Message/message';
 export { default as Input } from './Form/Input';
+export { default as Tooltip } from './Feedback/Tooltip';
 
