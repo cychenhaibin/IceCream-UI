38ea108c3f6e3dd6d70169b04a9aa0079b06fa7c
haibin.chen <haibin.chen@shopee.com>
2025-05-17 20:58:18 +0800

    feat:部署

--- .umirc.ts
+++ .umirc.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'dumi';
+const repo = 'dumi-book-test';
 
 export default defineConfig({
   title: 'IceCream-UI',
@@ -6,5 +7,11 @@ export default defineConfig({
     'https://gw.alipayobjects.com/zos/bmw-prod/d3e3eb39-1cd7-4aa5-827c-877deced6b7e/lalxt4g3_w256_h256.png',
   logo: 'https://gw.alipayobjects.com/zos/bmw-prod/d3e3eb39-1cd7-4aa5-827c-877deced6b7e/lalxt4g3_w256_h256.png',
   outputPath: 'docs-dist',
+
+  devServer: {
+    port: 1998, // 自定义端口号
+  },
+  base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
   // more config: https://d.umijs.org/config
 });
