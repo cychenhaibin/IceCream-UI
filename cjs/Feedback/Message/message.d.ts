87e9f81e924752862d476c09cb8da3ef6e7dd120
cychenhaibin <3242477764@qq.com>
2025-05-17 14:44:35 +0000

    Deploying to build from @ cychenhaibin/IceCream-UI@dcd8c11b7ce745bd633d21d64764317f55fc7434 🚀

--- cjs/Feedback/Message/message.d.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-declare const _default: {
-    success: (content: React.ReactNode, duration?: number) => void;
-    warning: (content: React.ReactNode, duration?: number) => void;
-    error: (content: React.ReactNode, duration?: number) => void;
-    info: (content: React.ReactNode, duration?: number) => void;
-};
-export default _default;
--- esm/Foo/index.d.ts
+++
@@ -1,4 +0,0 @@
-declare const _default: ({ title }: {
-    title: string;
-}) => import("react/jsx-runtime").JSX.Element;
-export default _default;
